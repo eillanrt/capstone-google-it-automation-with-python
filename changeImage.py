c8fbe3bef0c4e63383b5cfe3d1f7251572ca6162
Allan Robert Tan <argt@duck.com>
2023-07-07 20:08:46 +0800

    add changeImage.py

---
+++ changeImage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from PIL import Image
+import os
+
+images_path = os.path.join('supplier-data', 'images')
+
+def correctify_image(image_path):
+  desired_size = (600, 400)
+  filename = os.path.basename(image_path)
+
+  with Image.open(image_path) as img:
+    img.resize(desired_size).convert('RGB').save(image_path, format='jpeg')
+
+def main():
+    images_files = [os.path.join(images_path, image_file) for image_file in os.listdir(images_path)]
+
+    for image_file in images_files:
+      correctify_image(image_file)
+      # print(image_file)
+
+
+if __name__ == '__main__':
+    main()
