a76cf54f72ee43250df32c966363f4444dcf9db3
Allan Robert Tan <argt@duck.com>
2023-07-08 14:59:25 +0800

    generate email and send it

--- report_email.py
+++ report_email.py
@@ -4,6 +4,7 @@ from run import parse_description_txt, descriptions_path
 
 import os
 import reports
+import emails
 
 def descriptions_to_paragraph():
   descriptions_files = [os.path.join(descriptions_path, file) for file in os.listdir(descriptions_path)]
@@ -17,7 +18,12 @@ def main():
   paragraph = descriptions_to_paragraph()
   report_title = 'Processed update {}'.format(date.today())
 
-  reports.generate_report(os.path.join('tmp', 'processed.pdf'), report_title, paragraph)
+  report_path = os.path.join('tmp', 'processed.pdf')
+
+  reports.generate_report(report_path, report_title, paragraph)
+
+  message = emails.generate_email('email1@example.com', 'email2@example.com', 'Upload Completed - Online Fruit Store', paragraph, report_path)
+  emails.send_email(message)
 
 
 if __name__ == '__main__':
